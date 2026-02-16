e201e4714fcd7f7e5261c00a71795ba3ba3821d9
Danil Baryshnikov <danil.baryshnikov@icloud.com>
2026-02-16 00:43:11 -0500

    fix(deploy): configure next.js for railway deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 768, 1024, 1280, 1600, 1920, 2560, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+  },
 };
 
 export default nextConfig;
